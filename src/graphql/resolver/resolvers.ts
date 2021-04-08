24a90544fc7d62a1dcf078e81c8eee3d923e39ae
Emily Déat <emily.deat@gmail.com>
2021-04-08 09:53:36 +0200

    Refacto resolver

--- src/graphql/resolver/resolvers.ts
+++ src/graphql/resolver/resolvers.ts
@@ -1,92 +1,22 @@
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import UserSchema from '../../models/user'
-
 import { getQuizzes, addQuiz, updateExistingQuiz, deleteOneQuiz } from './utilsQuiz'
-import { answerToQuiz } from './utilsUser'
+import { answerToQuiz, getUsers, getMyInfos, updateMood, registration, connection } from './utilsUser'
 
 import { IQuiz, IUser } from '../../type'
 
-import { JWT_SECRET } from '../../utils'
-
 const resolvers = {
   Query: {
-    users: async (): Promise<IUser[]> => {
-      try {
-        return await UserSchema.find()
-      } catch (error) {
-        throw new Error(
-          'Impossible de récupérer les étudiants, problème server.'
-        )
-      }
-    },
+    users: async (): Promise<IUser[]> => await getUsers(),
     quizzes: async (): Promise<IQuiz[]> => await getQuizzes(),
-    myInformations: async (_: any, args: any): Promise<IUser> => {
-      try {
-        const user = await UserSchema.findOne({ _id: args.id })
-        if (!user) {
-          throw new Error("Cette personne n'existe pas")
-        }
-        return user
-      } catch (error) {
-        throw new Error(
-          'Impossible de récupérer les infos personnelles, problème server.'
-        )
-      }
-    }
+    myInformations: async (_: any, args: any): Promise<IUser> => await getMyInfos(_, args),
   },
   Mutation: {
-    updateMoodStudent: async (_: any, args: any): Promise<IUser | null> => {
-      try {
-        const user = await UserSchema.findOne({ _id: args.id })
-        user?.moods.push(args.mood)
-        user?.save()
-        return user
-      } catch (error) {
-        throw new Error("Impossible d'ajouter un mood.")
-      }
-    },
+    updateMoodStudent: async (_: any, args: any): Promise<IUser | null> => await updateMood(_, args),
     createResponsesToQuizzes: async (_: any, args: any): Promise<boolean> => await answerToQuiz(_, args),
     createQuiz: async (_: any, args: any): Promise<IQuiz> => await addQuiz(_, args),
-    updateQuiz: async (_: any, args: any): Promise<IQuiz | null> => updateExistingQuiz(_, args),
-    deleteQuiz: async (_: any, args: any): Promise<IQuiz | null> => deleteOneQuiz(_, args),
-    signup: async (_: any, args: any) => {
-      const password = await bcrypt.hash(args.user.password, 10)
-      try {
-        const user = new UserSchema({
-          ...args.user,
-          password
-        })
-        await user.save()
-        const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
-          expiresIn: '1d'
-        })
-        return {
-          token,
-          user
-        }
-      } catch (error) {
-        console.error(error)
-        throw new Error("Impossible d'ajouter un étudiant.")
-      }
-    },
-    login: async (_: any, args: any) => {
-      const user = await UserSchema.findOne({ email: args.user.email })
-      if (!user) {
-        throw new Error('No such user found')
-      }
-      const valid = await bcrypt.compare(args.user.password, user.password)
-      if (!valid) {
-        throw new Error('Invalid password')
-      }
-      const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
-        expiresIn: '1d'
-      })
-      return {
-        token,
-        user
-      }
-    }
+    updateQuiz: async (_: any, args: any): Promise<IQuiz | null> => await updateExistingQuiz(_, args),
+    deleteQuiz: async (_: any, args: any): Promise<IQuiz | null> => await deleteOneQuiz(_, args),
+    signup: async (_: any, args: any) => await registration(_, args),
+    login: async (_: any, args: any) => await connection(_, args)
   }
 }
 export default resolvers
--- src/graphql/resolver/utilsUser.ts
+++ src/graphql/resolver/utilsUser.ts
@@ -1,4 +1,8 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import UserSchema from '../../models/user'
+
+import { JWT_SECRET } from '../../utils'
 
 export const answerToQuiz = async (_: any, args: any) => {
   try {
@@ -12,5 +16,79 @@ export const answerToQuiz = async (_: any, args: any) => {
     return true
   } catch (error) {
     throw new Error("Impossible d'ajouter une note à ce quiz.")
+  }
+}
+
+export const getUsers = async () => {
+  try {
+    return await UserSchema.find()
+  } catch (error) {
+    throw new Error(
+      'Impossible de récupérer les étudiants, problème server.'
+    )
+  }
+}
+
+export const getMyInfos = async (_: any, args: any) => {
+  try {
+    const user = await UserSchema.findOne({ _id: args.id })
+    if (!user) {
+      throw new Error("Cette personne n'existe pas")
+    }
+    return user
+  } catch (error) {
+    throw new Error(
+      'Impossible de récupérer les infos personnelles, problème server.'
+    )
+  }
+}
+
+export const updateMood = async (_: any, args: any) => {
+  try {
+    const user = await UserSchema.findOne({ _id: args.id })
+    user?.moods.push(args.mood)
+    user?.save()
+    return user
+  } catch (error) {
+    throw new Error("Impossible d'ajouter un mood.")
+  }
+}
+
+export const registration = async (_: any, args: any) => {
+  const password = await bcrypt.hash(args.user.password, 10)
+      try {
+        const user = new UserSchema({
+          ...args.user,
+          password
+        })
+        await user.save()
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
+          expiresIn: '1d'
+        })
+        return {
+          token,
+          user
+        }
+      } catch (error) {
+        console.error(error)
+        throw new Error("Impossible d'ajouter un étudiant.")
+      }
+}
+
+export const connection = async (_: any, args: any) => {
+  const user = await UserSchema.findOne({ email: args.user.email })
+  if (!user) {
+    throw new Error('No such user found')
+  }
+  const valid = await bcrypt.compare(args.user.password, user.password)
+  if (!valid) {
+    throw new Error('Invalid password')
+  }
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
+    expiresIn: '1d'
+  })
+  return {
+    token,
+    user
   }
 }
