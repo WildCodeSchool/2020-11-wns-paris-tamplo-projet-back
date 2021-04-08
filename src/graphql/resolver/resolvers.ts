f1ed2c1f56d00a1e90267e71ebabd6ea4b7b240b
Emily Déat <emily.deat@gmail.com>
2021-04-08 10:07:56 +0200

    Clean code

--- src/graphql/resolver/resolvers.ts
+++ src/graphql/resolver/resolvers.ts
@@ -1,22 +1,39 @@
-import { getQuizzes, addQuiz, updateExistingQuiz, deleteOneQuiz } from './utilsQuiz'
-import { answerToQuiz, getUsers, getMyInfos, updateMood, registration, connection } from './utilsUser'
+import {
+  getQuizzes,
+  addQuiz,
+  updateExistingQuiz,
+  deleteOneQuiz
+} from './utilsQuiz'
+import {
+  answerToQuiz,
+  getUsers,
+  getMyInfos,
+  updateMood,
+  registration,
+  connection
+} from './utilsUser'
 
 import { IQuiz, IUser } from '../../type'
 
 const resolvers = {
   Query: {
-    users: async (): Promise<IUser[]> => await getUsers(),
-    quizzes: async (): Promise<IQuiz[]> => await getQuizzes(),
-    myInformations: async (_: any, args: any): Promise<IUser> => await getMyInfos(_, args),
+    users: async (): Promise<IUser[]> => getUsers(),
+    quizzes: async (): Promise<IQuiz[]> => getQuizzes(),
+    myInformations: async (_: any, args: any): Promise<IUser> =>
+      getMyInfos(_, args)
   },
   Mutation: {
-    updateMoodStudent: async (_: any, args: any): Promise<IUser | null> => await updateMood(_, args),
-    createResponsesToQuizzes: async (_: any, args: any): Promise<boolean> => await answerToQuiz(_, args),
-    createQuiz: async (_: any, args: any): Promise<IQuiz> => await addQuiz(_, args),
-    updateQuiz: async (_: any, args: any): Promise<IQuiz | null> => await updateExistingQuiz(_, args),
-    deleteQuiz: async (_: any, args: any): Promise<IQuiz | null> => await deleteOneQuiz(_, args),
-    signup: async (_: any, args: any) => await registration(_, args),
-    login: async (_: any, args: any) => await connection(_, args)
+    updateMoodStudent: async (_: any, args: any): Promise<IUser | null> =>
+      updateMood(_, args),
+    createResponsesToQuizzes: async (_: any, args: any): Promise<boolean> =>
+      answerToQuiz(_, args),
+    createQuiz: async (_: any, args: any): Promise<IQuiz> => addQuiz(_, args),
+    updateQuiz: async (_: any, args: any): Promise<IQuiz | null> =>
+      updateExistingQuiz(_, args),
+    deleteQuiz: async (_: any, args: any): Promise<IQuiz | null> =>
+      deleteOneQuiz(_, args),
+    signup: async (_: any, args: any) => registration(_, args),
+    login: async (_: any, args: any) => connection(_, args)
   }
 }
 export default resolvers
